382f35baa815051083ad2fc39e162a5f338b8bc1
iamaccurate <vishalsankar18@gmail.com>
2022-10-06 02:27:29 +0530

    Implement comment deletion

--- server/routes/Comments.js
+++ server/routes/Comments.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Comments } = require("../models");
-const {validateToken} = require("../middlewares/AuthMiddleware")
+const { validateToken } = require("../middlewares/AuthMiddleware");
 
 router.get("/:postId", async (req, res) => {
   const postId = req.params.postId;
@@ -9,15 +9,24 @@ router.get("/:postId", async (req, res) => {
   res.json(comments);
 });
 
-//only when next() is called in middleware, the req moves forward
 router.post("/", validateToken, async (req, res) => {
   const comment = req.body;
   const username = req.user.username;
-
   comment.username = username;
-
   await Comments.create(comment);
   res.json(comment);
+});
+
+router.delete("/:commentId", validateToken, async (req, res) => {
+  const commentId = req.params.commentId;
+
+  await Comments.destroy({
+    where: {
+      id: commentId,
+    },
+  });
+  
+  res.json("Comment Deleted!");
 });
 
 module.exports = router;
