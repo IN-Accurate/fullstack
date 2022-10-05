224fb07504f4778bfcb49049e24f7c55ce5ee026
iamaccurate <vishalsankar18@gmail.com>
2022-10-05 21:49:06 +0530

    Add frontend for registration and login

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,8 @@ import {BrowserRouter as Router,Route,Routes,Link} from 'react-router-dom';
 import Home from "./pages/Home"
 import CreatePost from "./pages/CreatePost"
 import Post from "./pages/Post"
+import Registration from "./pages/Registration"
+import Login from "./pages/Login"
 
 function App() {
 
@@ -18,6 +20,8 @@ function App() {
       
       <Link to = "/createpost">Create A Post</Link>   
       <Link to = "/">Home Page</Link>
+      <Link to = "/login">Login</Link>
+      <Link to = "/registration">Registration</Link>
       
     </div>
       
@@ -25,6 +29,8 @@ function App() {
         <Route path="/" exact element={<Home/>} />
         <Route path="/createpost" exact element={<CreatePost/>} />
         <Route path="/post/:id" exact element={<Post/>} />
+        <Route path="/registration" exact element={<Registration/>} />
+        <Route path="/login" exact element={<Login/>} />
       </Routes>
 
      </Router>
---
+++ client/src/pages/Registration.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
+import {useNavigate} from 'react-router-dom';
+
+
+function Registration() {
+
+
+  let navigate = useNavigate();
+
+  const initialValues = {
+    username: "",
+    password: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string().min(3).max(15).required(),
+    password: Yup.string().min(8).max(20).required()
+  });
+
+
+  const onSubmit = (data) =>{
+
+    axios.post("http://localhost:3001/auth", data).then((response) => {
+        navigate("/");
+        console.log('Registered Successfully!');
+    });
+
+  };
+
+  return (
+    <div>
+          <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+
+          
+          <label htmlFor="inputUser">Username </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputCreatePost"
+            name="username"
+            placeholder="Username Here"
+          />
+
+         <label htmlFor="password">Password </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputCreatePost"
+            name="password"
+            type="password" 
+            placeholder="Password Here"
+          />
+
+          <button type="submit">Register</button>
+          
+        </Form>
+      </Formik>
+    </div>
+  )
+}
+
+export default Registration
