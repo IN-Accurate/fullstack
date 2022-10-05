8273384f144d02167cadb82c0cfe3a896363d3cd
iamaccurate <vishalsankar18@gmail.com>
2022-10-05 18:17:09 +0530

    Add form for creating posts

--- client/src/pages/CreatePost.js
+++ client/src/pages/CreatePost.js
@@ -1,9 +1,66 @@
-import React from 'react'
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import axios from "axios";
 
 function CreatePost() {
+  const initialValues = {
+    title: "",
+    postText: "",
+    username: "",
+  };
+
+  const validationSchema = Yup.object().shape({
+    title: Yup.string().required("You must input a Title!"),
+    postText: Yup.string().required(),
+    username: Yup.string().min(3).max(15).required(),
+  });
+
+  const onSubmit = (data) => {
+    axios.post("http://localhost:3001/posts", data).then((response) => {
+      console.log("Posted Successfully !");
+    });
+  };
   return (
-    <div>CreatePost</div>
-  )
+    <div className="createPostPage">
+      <Formik
+        initialValues={initialValues}
+        onSubmit={onSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className="formContainer">
+
+          <label htmlFor="inputTitle">Title: </label>
+          <ErrorMessage name="title" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputTitle"
+            name="title"
+            placeholder="Title Here"
+          />
+          <label htmlFor ="inputPostText">Post</label>
+          <ErrorMessage name="postText" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputPostText"
+            name="postText"
+            placeholder="Post Text Here"
+          />
+          <label htmlFor="inputUser">Username </label>
+          <ErrorMessage name="username" component="span" />
+          <Field
+            autoComplete="off"
+            id="inputUser"
+            name="username"
+            placeholder="Username Here"
+          />
+
+          <button type="submit"> Create Post</button>
+          
+        </Form>
+      </Formik>
+    </div>
+  );
 }
 
-export default CreatePost
+export default CreatePost;
