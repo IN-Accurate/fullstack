b269399168ce266d34568abbf40a06ff0cbd70b8
iamaccurate <vishalsankar18@gmail.com>
2022-10-06 02:04:15 +0530

    Implement logout and show logged in username

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,11 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function App() {
-  const [authState, setAuthState] = useState(false);
+  const [authState, setAuthState] = useState({
+    username: "",
+    id: 0,
+    status: false,
+  });
 
   useEffect(() => {
     axios
@@ -21,49 +25,51 @@ function App() {
       })
       .then((response) => {
         if (response.data.error) {
-          setAuthState(false);
+          setAuthState({ ...authState, status: false });
         } else {
-          setAuthState(true);
+          setAuthState({
+            username: response.data.username,
+            id: response.data.id,
+            status: true,
+          });
         }
       });
   }, []);
 
+  const logout = () => {
+    localStorage.removeItem("accessToken");
+    setAuthState({ username: "", id: 0, status: false });
+  };
+
   return (
-
     <div className="App">
-
       <AuthContext.Provider value={{ authState, setAuthState }}>
-      
         <Router>
-
           <div className="navbar">
-         
-            <Link to="/"> Home Page</Link>
-            <Link to="/createpost"> Create A Post</Link>
-           
-            {!authState && (
-              <>
-                <Link to="/login"> Login</Link>
-                <Link to="/registration"> Registration</Link>
-              </>
-            )}
-
+            <div className="links">
+              <Link to="/"> Home Page</Link>
+              <Link to="/createpost"> Create A Post</Link>
+              {!authState.status && (
+                <>
+                  <Link to="/login"> Login</Link>
+                  <Link to="/registration"> Registration</Link>
+                </>
+              )}
+            </div>
+            <div className="loggedInContainer">
+              <h1>{authState.username} </h1>
+              {authState.status && <button onClick={logout}> Logout</button>}
+            </div>
           </div>
-
           <Routes>
-
             <Route path="/" exact element={<Home/>} />
-            <Route path="/createpost" exact element={<CreatePost/>}/>
-            <Route path="/post/:id" exact element={<Post/>} />
+            <Route path="/createpost" exact element={<CreatePost/>} />
+            <Route path="/post/:id" exact element={<Post/>}/>
             <Route path="/registration" exact element={<Registration/>} />
-            <Route path="/login" exact element = {<Login/>} />
-         
+            <Route path="/login" exact element={<Login/>} />
           </Routes>
-
         </Router>
-
       </AuthContext.Provider>
-      
     </div>
   );
 }
--- server/routes/Users.js
+++ server/routes/Users.js
@@ -1,76 +1,41 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {Users} = require('../models')
-const bcrypt = require('bcrypt');
+const { Users } = require("../models");
+const bcrypt = require("bcrypt");
+const { validateToken } = require("../middlewares/AuthMiddleware");
+const { sign } = require("jsonwebtoken");
 
-const {sign} = require('jsonwebtoken');
-
-const {validateToken} = require('../middlewares/AuthMiddleware')
-
-//https://stackoverflow.com/questions/31309759/what-is-secret-key-for-jwt-based-authentication-and-how-to-generate-it
-
-// User registration
-
-router.post('/',async(req, res) => {
-
-    //destructure the password and username individually        
-   
-    const {username,password} = req.body;
-
-    bcrypt.hash(password, 10).then((hash)=>
-    {
-        Users.create({
-            username: username,
-            password:hash
-        });
-
-        res.json("Registration Success!");
-    }); 
-    
-    // 10 is saltrounds - how long it takes to hash the password
-    // use bcrypt package for hashing the password
-
-    //The password hashing is one way, i.e, we can't get the original password from a hashed data
-    //Only thing we can do is whenever user enters a pw , just compare it with a hash that exits in the db
-
+router.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  bcrypt.hash(password, 10).then((hash) => {
+    Users.create({
+      username: username,
+      password: hash,
+    });
+    res.json("SUCCESS");
+  });
 });
 
-router.post('/login', async(req, res) => {
-    
-    const{username, password} = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
 
-    const user = await Users.findOne({where : {username: username }});
+  const user = await Users.findOne({ where: { username: username } });
 
-    if(!user)  // The user does not exist in our db
-       res.json({error:"User Doesn't Exist"});
-    
-    bcrypt.compare(password, user.password).then((match)=>
-        {
+  if (!user) res.json({ error: "User Doesn't Exist" });
 
-            if(!match) 
-              res.json({error:"Wrong username or password!"});
+  bcrypt.compare(password, user.password).then(async (match) => {
+    if (!match) res.json({ error: "Wrong Username And Password Combination" });
 
-            else{
-
-                 const accessToken = sign({username:user.username,id:user.id},
-                     "2745e24ab694a6ae01a842aa52a2ce6dab5f3f6d80f98002c06c4310b12391fb"
-                     );
-                     // node -e "console.log(require('crypto').randomBytes(32).toString('hex'))"
-                    
-                 res.json(accessToken);
-            
-            }
-
-            // In Google Chrome , Network tab gives all requests present currently. In the respone tab , there will be this token
-            // In application tab , we can get sessionstorage and cookie items
-            //JWT isn't 100% secure and is vulnerable to XSS attacks
-
-        });
-
+    const accessToken = sign(
+      { username: user.username, id: user.id },
+      "importantsecret"
+    );
+    res.json({ token: accessToken, username: username, id: user.id });
+  });
 });
 
-router.get('/auth',validateToken,(req, res) => {
-    res.json(req.user)
-})
+router.get("/auth", validateToken, (req, res) => {
+  res.json(req.user);
+});
 
 module.exports = router;
